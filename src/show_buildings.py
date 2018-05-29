c1330b0c31365c65f4a3da5f4cdff09d76c13e8e
Oleksandr Huziy <guziy.sasha@gmail.com>
2018-05-29 17:39:59 -0400

    Initial commit

---
+++ src/show_buildings.py
@@ -0,0 +1,11 @@
+import osmnx as ox
+
+# Retrieve OSM data
+from urbansprawl.core import get_processed_osm_data
+
+region_args = {"place":"Paris, France"}
+df_osm_built, df_osm_building_parts, df_osm_pois = get_processed_osm_data(city_ref_file = 'Paris_France', region_args = region_args)
+
+# Plot
+ax = df_osm_built.plot()
+ax.get_xaxis().set_visible(False);ax.get_yaxis().set_visible(False)
