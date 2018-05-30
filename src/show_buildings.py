493556d2e6df15691ef5d86d3ef3bb94f0ca34b9
Oleksandr Huziy <guziy.sasha@gmail.com>
2018-05-29 22:45:43 -0400

    Try make it running, but get some errors for json

--- src/show_buildings.py
+++ src/show_buildings.py
@@ -6,13 +6,12 @@ from urbansprawl.core import get_processed_osm_data
 
 
 def main():
-    region_args = {"place": "Paris, France"}
-    df_osm_built, df_osm_building_parts, df_osm_pois = get_processed_osm_data(city_ref_file='Paris_France',
-                                                                              region_args=region_args)
+    region_args = {"place": "Canada, Montreal"}
+    df_osm_built, df_osm_building_parts, df_osm_pois = get_processed_osm_data(region_args=region_args)
 
     # Plot
     ax = df_osm_built.plot()
-    ax.get_xaxis().set_visible(False);
+    ax.get_xaxis().set_visible(False)
     ax.get_yaxis().set_visible(False)
 
 
