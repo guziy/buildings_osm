0e7959be725cd339d5d3e0083afe22a34227a73c
Oleksandr Huziy <guziy.sasha@gmail.com>
2018-06-11 21:32:20 -0300

    wip

--- src/plot_annual_max_temperatures_for_cities.py
+++ src/plot_annual_max_temperatures_for_cities.py
@@ -1,9 +1,32 @@
 from collections import OrderedDict
 from pathlib import Path
 from cartopy import crs as ccrs
+import xarray
+import matplotlib.pyplot as plt
 
 def calculate_annual_max_temperature(cities: dict, data_root: Path):
     # calculate for each year
+
+    for yr_dir in data_root.iterdir():
+
+        if not yr_dir.is_dir():
+            continue
+
+        data_files = yr_dir / "MODIS-C06__MOD11C1__DAILY__LandSurfaceTemperature__0.05deg__UHAM-ICDC__20001218*.nc4"
+
+        data_files_s = str(data_files)
+
+        with xarray.open_mfdataset(data_files_s) as ds:
+
+            arr_data = ds["lst_day"].to_masked_array()
+
+            plt.figure()
+            cs = plt.contourf(arr_data)
+            plt.colorbar(cs)
+            plt.show()
+
+            if True:
+                raise Exception
 
     # get the average over available years
 
@@ -13,9 +36,6 @@ def calculate_annual_max_temperature(cities: dict, data_root: Path):
 
 def plot_field_for_cities(cities_extents: dict,
                           field, data_lons, data_lats):
-
-
-
     pass
 
 
