d211bed316e9e5a6cce47102639a4c524f9ade55
Oleksandr Huziy <guziy.sasha@gmail.com>
2018-06-11 22:17:54 -0300

    wip

--- src/plot_annual_max_temperatures_for_cities.py
+++ src/plot_annual_max_temperatures_for_cities.py
@@ -3,9 +3,17 @@ from pathlib import Path
 from cartopy import crs as ccrs
 import xarray
 import matplotlib.pyplot as plt
+from matplotlib.gridspec import GridSpec
+
+from show_buildings import download_buildings_cached_for, add_river_shapes
+import numpy as np
+
 
 def calculate_annual_max_temperature(data_root: Path):
     # calculate for each year
+    data_list = []
+    lons = None
+    lats = None
 
     for yr_dir in data_root.iterdir():
 
@@ -18,38 +26,93 @@ def calculate_annual_max_temperature(data_root: Path):
 
         data_files_s = str(data_files)
 
+
         with xarray.open_mfdataset(data_files_s) as ds:
 
             print(ds["lst_day"])
 
             arr_data = ds["lst_day"].max(dim="time").to_masked_array().squeeze()
 
-            plt.figure()
-            cs = plt.contourf(arr_data.T)
-            plt.colorbar(cs)
-            plt.savefig("test_plot.png")
+            if lons is None:
+                lons = ds["lons"].values
+                lats = ds["lats"].values
 
-            if True:
-                raise Exception
+            data_list.append(
+                np.ma.masked_where(arr_data >= 200, arr_data)
+            )
 
     # get the average over available years
+    arr_mean = np.ma.mean(data_list, axis=0)
 
     # return the field to plot
-    pass
+    return lons, lats, arr_mean
 
-
-def plot_field_for_cities(cities_extents: dict,
-                          field, data_lons, data_lats):
-    pass
 
 
 def main(cities: dict, radius_m=20000,
-         projection=ccrs.Miller()):
+         projection: ccrs.Projection=ccrs.Miller()):
 
     data_root = Path("/scratch/huziy/MODIS_hambourg/DAILY")
 
-    tmax_mean = calculate_annual_max_temperature(data_root=data_root)
+    lons, lats, tmax_mean = calculate_annual_max_temperature(data_root=data_root)
 
+
+    # Define the CartoPy CRS object.
+    crs = projection
+    # This can be converted into a `proj4` string/dict compatible with GeoPandas
+    crs_proj4 = projection.proj4_init
+
+
+    city_to_extent = OrderedDict()
+
+    # get the data and compute some things
+    for ci, city in enumerate(cities):
+        print(f"Downloading {city}")
+        try:
+            # blds = ox.buildings_from_place(city)
+            blds = download_buildings_cached_for(city, cities, radius_m=radius_m)
+        except TypeError:
+            continue
+
+        # blds.plot(facecolor="indigo", ax=ax)
+
+        blds = blds.to_crs(crs_proj4)
+        minx, miny, maxx, maxy = blds.total_bounds
+        city_to_extent[city] = [minx, maxx, miny, maxy]
+
+
+    ncols = 3
+    nrows = len(cities) // 3 + int(len(cities) % ncols > 0)
+    gs = GridSpec(nrows=nrows, ncols=ncols, width_ratios=[1,] * ncols, height_ratios=[1,] * nrows,
+                  wspace=0.5, hspace=0.5)
+
+    fig = plt.figure(figsize=(6, 6), frameon=False)
+
+    # plotting
+    for ci, city in enumerate(cities):
+        row = ci // ncols
+        col = ci % ncols
+
+        ax = fig.add_subplot(gs[row, col], projection=projection)
+
+
+        # Plot
+        print(f"Plotting {city}")
+
+
+        cs = ax.contourf(lons, lats, tmax_mean)
+        plt.colorbar(cs)
+
+        ax.set_extent(city_to_extent[city], crs=projection)
+
+        ax.get_xaxis().set_visible(False)
+        ax.get_yaxis().set_visible(False)
+        ax.outline_patch.set_visible(False)
+        ax.background_patch.set_visible(False)
+
+        add_river_shapes(ax, city, crs=ccrs.PlateCarree())
+
+    fig.savefig("cities_tmax.png", dpi=300, bbox_inches="tight")
 
 def test():
     cities = OrderedDict([
