62bf6be522549f9fe6c8dcd3d99de2dd3f8faab2
Oleksandr Huziy <guziy.sasha@gmail.com>
2018-06-14 18:14:04 -0300

    wip

--- src/plot_annual_max_temperatures_for_cities.py
+++ src/plot_annual_max_temperatures_for_cities.py
@@ -30,6 +30,7 @@ def plot_total_field(lons, lats, the_field, label="", vname="lst_day", projectio
     ax = plt.axes(projection=projection)
     cs = ax.pcolormesh(lons, lats, the_field, transform=projection, cmap=cmap, norm=bn)
     plt.colorbar(cs, extend="both", ax=ax)
+    ax.set_title(label)
     fig.savefig(f"total_field_{vname}_{label}.png", bbox_inches="tight")
 
 
